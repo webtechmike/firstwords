9869861148d090789cbeb6338a11574768824639
Mike Bergen <webtechmike@gmail.com>
2014-09-21 20:35:12 -0700

    add basic nav outline to app scaffold

---
+++ app.js
@@ -0,0 +1,25 @@
+angular.module('firstwords', ['ui.bootstrap','ui.utils','ui.router','ngAnimate']);
+
+angular.module('firstwords').config(function($stateProvider, $urlRouterProvider) {
+
+
+
+    /* Add New States Above */
+    $urlRouterProvider.otherwise('/home');
+
+});
+
+angular.module('firstwords').run(function($rootScope) {
+
+    $rootScope.safeApply = function(fn) {
+        var phase = $rootScope.$$phase;
+        if (phase === '$apply' || phase === '$digest') {
+            if (fn && (typeof(fn) === 'function')) {
+                fn();
+            }
+        } else {
+            this.$apply(fn);
+        }
+    };
+
+});
