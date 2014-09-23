bc16bec054570aaaae41d69c77afb4e7575b5279
Mike Bergen <webtechmike@gmail.com>
2014-09-23 10:10:10 -0700

    clean up layout - add contact and frames pages

--- app.js
+++ app.js
@@ -3,22 +3,30 @@ angular.module('firstwords', ['ui.bootstrap','ui.utils','ui.router','ngAnimate']
 angular.module('firstwords').config(function($stateProvider, $urlRouterProvider) {
 
     $stateProvider
-    .state('shop', {
-        url: '/shop',
-        templateUrl: 'partials/shop.html'
-    })
-    .state('service', {
-        url: '/service',
-        templateUrl: 'partials/service.html'
-    })
-    .state('contact', {
-        url: '/contact',
-        templateUrl: 'partials/contact.html'
-    })
-    .state('policies', {
-        url: '/policies',
-        templateUrl: 'partials/policies.html'
-    });
+        .state('shop', {
+            url: '/shop',
+            templateUrl: 'partials/shop.html'
+        })
+        .state('frames', {
+            url: '/frames',
+            templateUrl: 'partials/frames.html'
+        })
+        .state('service', {
+            url: '/service',
+            templateUrl: 'partials/service.html'
+        })
+        .state('contact', {
+            url: '/contact',
+            templateUrl: 'partials/contact.html'
+        })
+        .state('policies', {
+            url: '/policies',
+            templateUrl: 'partials/policies.html'
+        })
+        .state('news', {
+            url: '/news',
+            templateUrl: 'partials/news.html'
+        });
 
     /* Add New States Above */
     $urlRouterProvider.otherwise('/home');
@@ -38,4 +46,22 @@ angular.module('firstwords').run(function($rootScope) {
         }
     };
 
+});
+
+angular.directive('videoPlayer', function() {
+    return {
+        restrict: "E",
+        play: function() {
+            // play video
+        }
+    };
+});
+
+angular.directive('carousel', function ($scope) {
+    return {
+        restrict: "E",
+        move: function($scope, target, direction) {
+            $($scope + ' ' + target).addClass('move' + direction);
+        }
+    };
 });
